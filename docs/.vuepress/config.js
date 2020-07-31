6e8b26219b1d5d506810f371f496a34b9ceee5e1
YOYZHANG <alexuzhang@gmail.com>
2020-07-31 22:56:14 +0800

    update script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,6 @@
-const readFileList = require("../scripts/build.js");
 module.exports = {
     title: '每周时报',
     description: '每周更新前端前沿技术',
-    head: [
-        'link',
-        {
-            rel: 'alternate',
-            type: 'application/rss+xml',
-            href: '/rss.xml',
-            title: '每周时报'
-        }
-    ],
     lastUpdated: "Last Updated",
     themeConfig: {
         nav: [
@@ -21,7 +11,7 @@ module.exports = {
             {
                 title: "2020年07月",
                 collapsable: false,
-                children: readFileList("2020", "07")
+                children: [['/2020/07/31.md', '2020年07月31日']]
             }
         ],
     }
